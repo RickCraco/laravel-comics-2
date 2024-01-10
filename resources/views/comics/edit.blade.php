e1c64832e58d1b4ee6843781ffe03618b35b6f2e
RickCraco <riccardocracolici43@gmail.com>
2024-01-10 16:44:43 +0100

    fixed problem + added errors to edit page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,6 +5,15 @@
 @section('content')
 <main class="bg-dark">
     <section class="container mb-4">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
         <form action="{{route('comics.update',$comic)}}" method="POST">
             @csrf
             @method('PUT')
